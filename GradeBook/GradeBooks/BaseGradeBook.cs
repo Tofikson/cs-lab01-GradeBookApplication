0b5e7354a361fe1e5754865377fe287bd6e9c1f6
Tofikson <79985872+Tofikson@users.noreply.github.com>
2022-03-10 21:16:29 +0100

    GradeBookType Type

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,6 +11,7 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+        public GradeBookType Type { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
@@ -18,6 +19,7 @@ namespace GradeBook.GradeBooks
         {
             Name = name;
             Students = new List<Student>();
+            Type = Type;
         }
 
         public void AddStudent(Student student)
