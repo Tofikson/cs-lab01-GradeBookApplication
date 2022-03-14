76e64b00f7be901ebdccedbedf5091ec09beba2b
Tofikson <79985872+Tofikson@users.noreply.github.com>
2022-03-14 18:54:49 +0100

    Ranked and Standard GradeBook

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+        //public override char GetLetterGrade(double averageGrade)
+        //{
+        //    if (Students.Count < 5)
+        //        throw new InvalidOperationException();
+        //    else if (averageGrade => )
+        //        return 'A';
+        //}
+    }
+}
