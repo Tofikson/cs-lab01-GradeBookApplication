45bf8c5c17bd3c84d2ae3406c22b66e4b98e6d17
Tofikson <79985872+Tofikson@users.noreply.github.com>
2022-03-31 21:00:24 +0200

    Override GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,11 +14,25 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count() < 5)
+            if (Students.Count < 5)
                 throw new InvalidOperationException();
-            else if (averageGrade >= student.averageGrade )
+            int betterStudents = 0;
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (Students[i].AverageGrade > averageGrade)
+                    betterStudents++;
+            }
+            double ranking = (double)(betterStudents + 1) / Students.Count;
+            if (ranking <= 0.2)
                 return 'A';
-
+            if (ranking <= 0.4)
+                return 'B';
+            if (ranking <= 0.6)
+                return 'C';
+            if (ranking <= 0.8)
+                return 'D';
+            else return 'F';
         }
+
     }
 }
